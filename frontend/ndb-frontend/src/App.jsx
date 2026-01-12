5ec496efefc8db6e656171af81492867c3200486
Fabien Le Berre <fleberre.pro@gmail.com>
2026-01-12 18:08:00 +0100

    chore(frontend): add env config and api base url

--- frontend/ndb-frontend/src/App.jsx
+++ frontend/ndb-frontend/src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
+  const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
   return (
     <div>
       <h1>Nuit du Basket – Frontend</h1>
+      <p>API base URL : {apiBaseUrl}</p>
     </div>
   );
 }
